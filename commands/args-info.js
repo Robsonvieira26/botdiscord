528a83531ee487ec569c8a42711533cf6258e078
rosbsonroberto99@gmail.com <robson@TARS.localdomain>
2020-08-18 11:05:50 -0300

    comandos de ajuda e informações adicionados

--- commands/args-info.js
+++ commands/args-info.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'args-info',
-	description: 'Information about the arguments provided.',
+	description: 'Teste leitura de argumentos',
 	execute(message, args) {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
